810d70f3d42fb2a66d2dc9a070156b459366161a
ripterdust <bryantello2010@hotmail.com>
2020-11-10 16:14:04 -0600

    Viendo como agrego atributos a la clase Notes

---
+++ frontend/src/components/notes/notes.js
@@ -0,0 +1,16 @@
+import { Component } from 'react';
+import Axios from 'axios';
+
+class Note extends Component {
+    this.name = []
+    
+    get Notes(){
+
+    }
+
+    render(){
+        return <div>hola</div>
+    }
+}
+
+export default Note;
